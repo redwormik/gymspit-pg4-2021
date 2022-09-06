728e0eaa20b5660d8f3280e45371b9583bb12694
redwormik <redwormik@gmail.com>
2022-09-06 08:57:21 +0200

    Add Lecture1

---
+++ Lecture1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Lecture1
+{
+	class Program
+	{
+		static void PrintHighways(City city)
+		{
+			Console.WriteLine("{0}:", city.Name);
+			foreach (Highway highway in city.GetHighways()) {
+				Console.WriteLine("\t{0} ({1} km):", highway.OtherEnd(city).Name, highway.Length);
+			}
+			Console.WriteLine();
+		}
+
+
+		static void Main(string[] args)
+		{
+			City praha = new City("Praha");
+			City brno = new City("Brno");
+			City bratislava = new City("Bratislava");
+
+			praha.AddHighway(brno, 200);
+			brno.AddHighway(bratislava, 100);
+
+			PrintHighways(praha);
+			PrintHighways(brno);
+			PrintHighways(bratislava);
+
+			Console.WriteLine("Press any key to quit...");
+			Console.ReadKey();
+		}
+	}
+}
