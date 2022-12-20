a65bbe11061f06cb69df058415ddd7a92c32c290
Jakub Červenka <redwormik@gmail.com>
2022-12-20 08:22:55 +0100

    Add Lecture14 project and model

---
+++ Lecture14/Lecture14/Model/Ingredient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace Lecture14.Model
+{
+	public class Ingredient: INotifyPropertyChanged
+	{
+		private string _name;
+		public string Name
+		{
+			get {
+				return _name;
+			}
+
+			set {
+				_name = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+
+		public Ingredient(int? Id, string Name)
+		{
+			_name = Name;
+		}
+
+
+		protected void OnPropertyChanged([CallerMemberName] string name = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+	}
+}
---
+++ Lecture14/Lecture14/Model/Recipe.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace Lecture14.Model
+{
+	public class Recipe: INotifyPropertyChanged
+	{
+		private string _Name;
+		public string Name
+		{
+			get {
+				return _Name;
+			}
+
+			set {
+				_Name = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private string _Description;
+		public string Description
+		{
+			get {
+				return _Description;
+			}
+
+			set {
+				_Description = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public ObservableCollection<Ingredient> Ingredients
+		{
+			get;
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+
+		public Recipe(string Name, string Description)
+		{
+			_Name = Name;
+			_Description = Description;
+		}
+
+
+		public void AddIngredient(Ingredient ingredient)
+		{
+			Ingredients.Add(ingredient);
+			OnPropertyChanged("Ingredients");
+		}
+
+
+		public void RemoveIngredient(Ingredient ingredient)
+		{
+			Ingredients.Remove(ingredient);
+			OnPropertyChanged("Ingredients");
+		}
+
+
+		protected void OnPropertyChanged([CallerMemberName] string name = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+	}
+}
